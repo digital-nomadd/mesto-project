fc24434cd9583d6ac49b1c669e97e94415f4986d
digital-nomadd <fomin.vladislav.mlg@gmail.com>
2022-11-26 22:50:18 +0300

    added all func, first code-review iteration

--- script.js
+++ script.js
@@ -1,40 +1,152 @@
-// кладем в переменную контейнер 
-const main = document.querySelector('.main'); 
+// ########## Открытие Попапов ##########
 
-// кладем в переменную кнопку edit-profile
-const profileEditButton = main.querySelector('.profile__edit-button');
+// ---------- Открытие Edit popup со значениями----------
+const profileEditButtonElement = document.querySelector('.profile__edit-button');
+const profileEditPopupElement = document.querySelector('.popup_type_edit'); 
 
-// кладем в переменную popup-форму
-const profileEditPopup = document.querySelector('.popup'); 
+const profileTitleElement = document.querySelector('.profile__title');
+const profileSubtitleElement = document.querySelector('.profile__subtitle');
 
-// кладем в переменную кнопку закрытия popup-формы
-const profileСloseButton = document.querySelector('.popup__close-button');
+const profileEditPopupFormElement = profileEditPopupElement.querySelector('.popup__form');
+const profileEditPopupNameElement = profileEditPopupElement.querySelector('.popup__input_el_name'); 
+const profileEditPopupAboutElement = profileEditPopupElement.querySelector('.popup__input_el_about');
 
-// функция toggle для popup-формы
-function togglePopup() {
-  profileEditPopup.classList.toggle('popup_opened');
+profileEditButtonElement.addEventListener('click', function() {              
+  openPopup(profileEditPopupElement);                                       
+  profileEditPopupNameElement.value = profileTitleElement.textContent;      
+  profileEditPopupAboutElement.value = profileSubtitleElement.textContent;
+});
+
+// ---------- Открытие Add popup ----------
+const profileAddPlaceButtonElement = document.querySelector('.profile__button');
+const profileAddPlacePopupElement = document.querySelector('.popup_type_add');
+const profileAddPlaceFormElement = profileAddPlacePopupElement.querySelector('.popup__form');
+const profileAddPlaceTitleElement = profileAddPlacePopupElement.querySelector('.popup__input_el_place');
+const profileAddPlaceLinkElement = profileAddPlacePopupElement.querySelector('.popup__input_el_link');
+
+profileAddPlaceButtonElement.addEventListener('click', function() {
+  openPopup(profileAddPlacePopupElement);
+});
+
+// ----------Сама Функция открытия ----------
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+} 
+
+// ########## Закрытие Попапов ##########
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-// слушатель событий для кнопки edit-profile
-profileEditButton.addEventListener('click', togglePopup);
+const closeButtonElements = document.querySelectorAll('.popup__close-button');
 
-// слушатель событий для кнопки popup__close-button
-profileСloseButton.addEventListener('click', togglePopup);
-
-
-
+closeButtonElements.forEach(closeBtn => {
+  const popup = closeBtn.closest('.popup');
+  closeBtn.addEventListener('click', () => closePopup(popup));
+});
 
 
+// ########## Сохранение введенных данных Edit-popup ##########
 
-
-
-
-const cardLikeButton = main.querySelectorAll('.card__button');
-console.log(cardLikeButton);
-
-// функция toggle для кнопки лайка на карточках
-function toggleLikeButton() {
-  cardLikeButton.classList.toggle('card__button_active');
+function saveEditPopupData(evt) {
+  evt.preventDefault(); // убираем дефолтную перезагрузку страницы
+  profileTitleElement.textContent = profileEditPopupNameElement.value;
+  profileSubtitleElement.textContent = profileEditPopupAboutElement.value;
+  closePopup(profileEditPopupElement);
 }
 
-cardLikeButton.addEventListener('click', toggleLikeButton);
+profileEditPopupFormElement.addEventListener('submit', saveEditPopupData);
+
+
+// ########## Отображение 6 карточек из коробки ##########
+
+
+
+const cards = document.querySelector('.cards');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/* Находим и задаем переменные попапа с отображением картинки на карточке */
+const popupPlace = document.querySelector('.popup-place');
+const popupPlaceImage = popupPlace.querySelector('.popup-place__image');
+const popupPlaceCaption = popupPlace.querySelector('.popup-place__caption');
+
+/*Функция открытия картинки*/
+function openImagePopup (image, caption) {
+  openPopup(popupPlace);
+  popupPlaceImage.setAttribute('src', image);
+  popupPlaceCaption.textContent = caption;
+  popupPlaceImage.setAttribute('alt', caption);
+}
+
+function createCard(src, alt) {
+  const cardTemplateElement = document.querySelector('#card-template').content;
+  const cardElement = cardTemplateElement.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardImage.setAttribute('src', src);
+  cardImage.setAttribute('alt', alt);
+  cardElement.querySelector('.card__title').textContent = alt;
+  /*Слушатель событий на иконку лайка на карточке*/
+  cardElement.querySelector('.card__button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__button_active');   // реализайция лайка
+  });
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+    evt.target.closest('.card').remove();
+  });
+    /*Слушатель событий на картинку карточки, при клике на которую запускается функция openImagePopup*/
+    cardImage.addEventListener('click', function (evt) {
+    openImagePopup(src, alt);
+  });
+  return cardElement;
+}
+
+/*Добавление новой карточки*/
+function addNewCard(src, alt) {
+  const card = createCard(src, alt);
+  cards.prepend(card);
+}
+
+/*Карточки из коробки*/
+initialCards.forEach(function (item) {
+  addNewCard(item.link, item.name);
+});
+
+/*Добавление карточки вручную*/
+function saveCard(evt) {
+  evt.preventDefault();
+  const title = profileAddPlaceTitleElement.value;
+  const link = profileAddPlaceLinkElement.value;
+  addNewCard(title, link);
+  evt.target.reset();
+
+  closePopup(profileAddPlacePopupElement);
+}
+
+profileAddPlaceFormElement.addEventListener('submit', saveCard);
